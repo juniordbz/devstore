7e94f8961dddddb20df6f418e54031a6a8cc43f3
juniordbz <franciscojunior02@hotmail.com.br>
2024-05-25 19:17:49 -0300

    add header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html className={inter.variable} lang="en">
-      <body>{children}</body>
+    <html lang="en" className={inter.className}>
+      <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
     </html>
   )
 }
