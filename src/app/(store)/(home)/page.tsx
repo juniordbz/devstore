1e8b572c5e5a8548ffd4b0310ce7e6b904f29590
juniordbz <franciscojunior02@hotmail.com.br>
2024-05-30 19:34:16 -0300

    next revalidate - cash & memoization

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -2,10 +2,13 @@ import { api } from '@/data/api'
 import { Product } from '@/data/types/product'
 import Image from 'next/image'
 import Link from 'next/link'
-import style from 'styled-jsx/style'
 
 async function getFeaturedProducts(): Promise<Product[]> {
-  const response = await api('/products/featured')
+  const response = await api('/products/featured', {
+    next: {
+      revalidate: 60 * 60, // mantem dados estáticos por 1Hora
+    },
+  })
 
   const products = await response.json()
 
